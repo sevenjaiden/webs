9f4799022303cb6179cda0074166b20679adb6b9
sevenjaiden <157500764+sevenjaiden@users.noreply.github.com>
2024-11-04 16:41:51 -0500

    added code from class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+import fetch from 'node-fetch'
 
 const express = require('express');
 const app = express();
@@ -11,9 +12,26 @@ app.get('/courses/:department/:level', (req, res) => {
 
 app.get('/clothing', (req, res) => {
     const { category, color } = req.query;
-    res.json({ message: `Showing all ${color} ${category} clothing items.` });
+    res.json({ message: `Showing all ${color} ${category}.` });
 });
 
 app.listen(PORT, () => {
     console.log(`Ready on port ${PORT}`);
 });
+
+// Example of an application route that makes a request to another server
+app.get('/advice', async (req, res) => {
+    // Make a request to another wbesite and wait for a response
+    const response = await fetch('https://api.adviceslip.com/advice')
+  
+    // Read the response
+    const body = await response.json()
+  
+    // Print the repsonse body to the console
+    console.log(body)
+  
+    // Get the advice text string from the response body object
+    const advice = body.slip.advice
+  
+    res.json({ data: advice })
+  })
