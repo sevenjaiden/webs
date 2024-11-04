dc169d3637f7eb946a5e275c1725021206975a57
sevenjaiden <157500764+sevenjaiden@users.noreply.github.com>
2024-11-04 17:18:09 -0500

    try

--- index.js
+++ index.js
@@ -21,21 +21,3 @@ app.get('/clothing', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Ready on port ${PORT}`);
 });
-
-
-// Example of an application route that makes a request to another server
-app.get('/advice', async (req, res) => {
-    // Make a request to another wbesite and wait for a response
-    const response = await fetch('https://api.adviceslip.com/advice')
-  
-    // Read the response
-    const body = await response.json()
-  
-    // Print the repsonse body to the console
-    console.log(body)
-  
-    // Get the advice text string from the response body object
-    const advice = body.slip.advice
-  
-    res.json({ data: advice })
-  })
