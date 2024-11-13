1e33354e218ac5854314878db9376379f2caaf10
sevenjaiden <157500764+sevenjaiden@users.noreply.github.com>
2024-11-13 13:17:49 -0500

    format change

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ app.use(cors({
     origin: '*'
 }));
 
-app.get('/courses/:department/:level', (req, res) => {
-    const { department, level } = req.params;
-    res.json({ department: department,
-                level: level});
+// playersJson.filter(player => player.name === req.)
+
+app.get('/players/:name/:number', (req, res) => {
+    const { name, number, } = req.params;
+    console.log("request",name, number)
+    res.json({ 
+        name: name,
+        number: number
+    });
 });
 
-app.get('/clothing', (req, res) => {
-    const { category, color } = req.query;
-    res.json({ result: `Showing all ${color} ${category}.` });
-});
+// const request = function() {
+//     fetch(`${baseUrl}/players/`)
+// }
 
 app.listen(PORT, () => {
     console.log(`Ready on port ${PORT}`);
